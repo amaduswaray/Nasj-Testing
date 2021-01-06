0e226786da733c14e4975a119ee634ed6b815781
adriawh <70323886+adriawh@users.noreply.github.com>
2021-01-06 19:39:01 +0100

    Initial Commit

---
+++ scroll/scrollApp.swift
@@ -0,0 +1,17 @@
+//
+//  scrollApp.swift
+//  scroll
+//
+//  Created by Adrian Hakvåg on 06/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct scrollApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
