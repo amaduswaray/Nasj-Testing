edcf454119f52cb837b0046255cc6dc4f33bd490
Adrian Wist Hakvåg <adrianwisthakvag@gmail.com>
2021-06-29 12:58:34 +0200

    chat.swift

---
+++ scroll/Chat.swift
@@ -0,0 +1,20 @@
+//
+//  Chat.swift
+//  scroll
+//
+//  Created by Adrian Hakvåg on 29/06/2021.
+//
+
+import SwiftUI
+
+struct Chat: View {
+    var body: some View {
+        
+    }
+}
+
+struct Chat_Previews: PreviewProvider {
+    static var previews: some View {
+        Chat()
+    }
+}
