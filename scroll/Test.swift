0334fd8626f96bddbd4cb42a79e2189fa686f904
Adrian Wist Hakvåg <adrianwisthakvag@gmail.com>
2021-06-29 11:16:10 +0200

    testview

---
+++ scroll/Test.swift
@@ -0,0 +1,27 @@
+//
+//  Test.swift
+//  scroll
+//
+//  Created by Adrian Hakvåg on 06/01/2021.
+//
+
+import Foundation
+import SwiftUI
+
+
+struct TestView: View{
+    
+    var body: some View{
+        Text("hei")
+        
+    }
+    
+}
+
+
+
+struct Test_Previews: PreviewProvider {
+    static var previews: some View {
+        /*@START_MENU_TOKEN@*/Text("Hello, World!")/*@END_MENU_TOKEN@*/
+    }
+}
